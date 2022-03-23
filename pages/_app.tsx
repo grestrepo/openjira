d6841e1580fb62a2e224698f958c200b8913d3d5
Gustavo Restrepo <39838724+tavorestrepo98@users.noreply.github.com>
2022-03-23 10:28:03 -0500

    implementación de materialUI, sidebar, navbar + uicontext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+
+import { UIProvider } from '../context/ui';
+import {darkTheme} from '../themes';
+import '../styles/globals.css';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UIProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UIProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
